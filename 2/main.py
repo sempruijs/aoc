3534f9fc4a7da9dcf73eda28a035e9cd5e1959e7
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-12-02 18:13:32 +0100

    Finish the first part

--- 2/main.py
+++ 2/main.py
@@ -1,20 +1,35 @@
 # Transform text to useful data
-data = []
+lines = []
 
 file = open("input.txt", "r")
 
 for line in file:
-    data.append(line.strip())
+    lines.append(line.strip())
 file.close()
+
+# amount of valid passwords
+valid_passwords = 0
 
 
 def valid_password(password, letter, min, max):
     times_in_password = 0
-
-    print(list(password))
 
     for character in list(password):
         if character == letter:
             times_in_password += 1
 
     return min <= times_in_password <= max
+
+
+for line in lines:
+    minmax = line.split(" ", 1)[0]
+    min = int(minmax.split("-")[0])
+    max = int(minmax.split("-")[1])
+
+    letter = list(line.split()[1])[0]
+    password = line.split()[2]
+
+    if valid_password(password, letter, min, max):
+        valid_passwords += 1
+
+print(valid_passwords)
