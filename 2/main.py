21fcf0b8dea718d154c11a39de3cedc8fadc1522
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-12-02 17:41:00 +0100

    Make a valid_password function

--- 2/main.py
+++ 2/main.py
@@ -7,3 +7,14 @@ for line in file:
     data.append(line.strip())
 file.close()
 
+
+def valid_password(password, letter, min, max):
+    times_in_password = 0
+
+    print(list(password))
+
+    for character in list(password):
+        if character == letter:
+            times_in_password += 1
+
+    return min <= times_in_password <= max
