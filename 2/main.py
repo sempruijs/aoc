9138311219f4e44b0b8b992ffa5927290942da2c
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-12-02 18:40:36 +0100

    Solve part 2

--- 2/main.py
+++ 2/main.py
@@ -8,10 +8,11 @@ for line in file:
 file.close()
 
 # amount of valid passwords
-valid_passwords = 0
+valid_passwords_one = 0
+valid_passwords_two = 0
 
 
-def valid_password(password, letter, min, max):
+def valid_password_one(password, letter, min, max):
     times_in_password = 0
 
     for character in list(password):
@@ -21,14 +22,24 @@ def valid_password(password, letter, min, max):
     return min <= times_in_password <= max
 
 
+def valid_password_two(password, letter, min, max):
+    return (list(password)[min - 1] == letter) ^ (list(password)[max - 1] == letter)
+
+
 for line in lines:
     minmax = line.split(" ", 1)[0]
     min, max = minmax.split("-")
+    min = int(min)
+    max = int(max)
 
     letter = list(line.split()[1])[0]
     password = line.split()[2]
 
-    if valid_password(password, letter, int(min), int(max)):
-        valid_passwords += 1
+    if valid_password_one(password, letter, min, max):
+        valid_passwords_one += 1
+    if valid_password_two(password, letter, min, max):
+        valid_passwords_two += 1
 
-print(valid_passwords)
+
+print(valid_passwords_one)
+print(valid_passwords_two)
