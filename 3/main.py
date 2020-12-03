9a5ad5691f0f66bcf0a38c0fd625a10ca7e59699
Sem Pruijs <19208988+SemPruijs@users.noreply.github.com>
2020-12-03 18:35:17 +0100

    Improve calculated_position function

--- 3/main.py
+++ 3/main.py
@@ -16,23 +16,23 @@ def encounter_tree(line, position):
     return list(line)[position] == "#"
 
 
-def calculated_position(position):
-    if position <= 27:
-        position += 3
-    elif position == 28:
-        position = 0
-    elif position == 29:
-        position = 1
-    elif position == 30:
-        position = 2
-
-    return position
+def calculated_position(right):
+    counter = current_position
+    right_used = right
+    while right_used > 0:
+        if counter == 30:
+            counter = 0
+            right_used -= 1
+        else:
+            counter += 1
+            right_used -= 1
+    return counter
 
 
 for line in lines:
     if encounter_tree(line, current_position):
         trees_encountert += 1
 
-    current_position = calculated_position(current_position)
+    current_position = calculated_position(3)
 
 print(trees_encountert)
