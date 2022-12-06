fa74b423a9e8287f7e71df62f1c5f17188f05eb2
cor <cor@pruijs.dev>
2022-12-06 19:12:12 +0100

    feat(2022/01): scaffold

---
+++ 2022/01/main.ts
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+fs.readFile('./input/example.txt', 'utf8', (err: any, data: string) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(data);
+});
