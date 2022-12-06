456e5d6d5cb320eb036e0203dac78b201f881813
Sem Pruijs <sem@pruijs.net>
2022-12-06 22:45:17 +0100

    solve 1.1

--- 2022/01/part1.ts
+++ 2022/01/part1.ts
@@ -1,9 +1,24 @@
 const fs = require('fs');
 
-fs.readFile('./input/example.txt', 'utf8', (err: any, data: string) => {
+fs.readFile('./input/input.txt', 'utf8', (err: any, data: string) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data);
+  const lines = data.split("\n")  
+
+  let evleValue = 0
+  let elvesValues = []
+
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i] != "") {
+      evleValue += Number(lines[i])
+    } else {
+      elvesValues.push(evleValue)
+      evleValue = 0
+    }
+  }
+  const sortedEvlesValues = elvesValues.sort((n1,n2) => n1 - n2)
+  console.log(sortedEvlesValues[sortedEvlesValues.length - 1])
 });
+
