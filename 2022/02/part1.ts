7b1aadee2c9341b291f19b8021ca37d9369779cd
Sem Pruijs <sem@pruijs.net>
2022-12-06 23:47:44 +0100

    Set up environment for 02

---
+++ 2022/02/part1.ts
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+fs.readFile('./input/example.txt', 'utf8', (err: any, data: string) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const lines = data.split("\n")  
+});
+
