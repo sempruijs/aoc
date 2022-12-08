3b6d8df438ef3ccd5f9b3604fea1f5db6dfdb138
Sem Pruijs <sem@pruijs.net>
2022-12-08 23:08:18 +0100

    Solve 3.2

--- 2022/03/part2.ts
+++ 2022/03/part2.ts
@@ -6,8 +6,8 @@ fs.readFile('./input/input.txt', 'utf8', (err: any, data: string) => {
     return;
   }
 
-  function chunks<T>(arr, chunkSize): Array<T> {
-    const res: Array<T> = [];
+  function chunks(arr: Array<string>, chunkSize: number): Array<Array<string>> {
+    const res: Array<Array<string>> = [];
     for (let i = 0; i < arr.length; i += chunkSize) {
         const chunk = arr.slice(i, i + chunkSize);
         res.push(chunk);
@@ -15,29 +15,35 @@ fs.readFile('./input/input.txt', 'utf8', (err: any, data: string) => {
     return res;
 }
 
-  function getMatchingChar(a: string, b: string): string {
-    for (let i = 0; i < a.length; i++) {
-      if (b.includes(a.charAt(i))) {
-        return a.charAt(i)
-      }
-    }
-    return "Error finding character"
+  function getMatchingChar(group: Array<string>) {
+    const a = stringToSet(group[0])
+    const b = stringToSet(group[1])
+    const c = stringToSet(group[2])
+
+    return [...intersectSets(a, intersectSets(b, c))][0]
   }
 
-  function getCompartments(line: string): Array<string> {
-    const compartmentLength = line.length / 2
-    return [line.slice(0, compartmentLength), line.slice(-compartmentLength)]
+  function stringToSet(input: string): Set<string> {
+    return new Set(Array.from(input))
   }
+
+  function intersectSets(a: Set<string>, b: Set<string>): Set<string> {
+    return new Set([...a].filter(i => b.has(i)))
+  }
+
 
   function charValue(char: string): number {
     return (parseInt(char, 36) - 9) + (char == char.toUpperCase() ? 26 : 0)
   }
 
-  function lineToValue(line: string): number {
-    return charValue(getMatchingChar(getCompartments(line)[0], getCompartments(line)[1]))
+  function groupToValue(group: Array<string>): number {
+    return charValue(getMatchingChar(group))
   }
+
   const lines = data.split("\n")
-  const totalPoints = lines.map((line) => lineToValue(line)).reduce((previous, current) => previous + current)
+  const groups = chunks(lines, 3)
+  const totalPoints = groups.map((group => groupToValue(group))).reduce((p, c) => p + c)
+
   console.log(totalPoints)
 });
 
