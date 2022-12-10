e3f2538b22c998050339c94bc86f8486b9ff279e
Sem Pruijs <sem@pruijs.net>
2022-12-10 20:22:27 +0100

    Set up enviourment for day 5

---
+++ 2022/05/part1.ts
@@ -0,0 +1,11 @@
+const fs = require('fs');
+
+fs.readFile('./input/example.txt', 'utf8', (err: any, data: string) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  const lines = data.split("\n")
+});
+
