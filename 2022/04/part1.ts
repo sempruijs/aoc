c17323b1d8ef7e81a66b4941e90823f9225d2ce2
Sem Pruijs <sem@pruijs.net>
2022-12-10 15:23:20 +0100

    Solve 4.1

--- 2022/04/part1.ts
+++ 2022/04/part1.ts
@@ -1,11 +1,23 @@
 const fs = require('fs');
 
-fs.readFile('./input/example.txt', 'utf8', (err: any, data: string) => {
+fs.readFile('./input/input.txt', 'utf8', (err: any, data: string) => {
   if (err) {
     console.error(err);
     return;
   }
 
+  function lineToTuples(line: string): Array<[number, number]> {
+    const tupleNumber = (a: number, b: number) => Number(line.split(",")[a].split("-")[b])
+    return [[tupleNumber(0,0), tupleNumber(0,1)], [tupleNumber(1,0), tupleNumber(1,1)]]
+  }
+
+  function tuplesContainUseless(tuples: Array<[number, number]>): boolean {
+    return (tuples[0][0] >= tuples[1][0] && tuples[0][1] <= tuples[1][1]) || (tuples[0][0] <= tuples[1][0] && tuples[0][1] >= tuples[1][1])
+  }
+
   const lines = data.split("\n")
+  const uselessJobsAmount = lines.map(line => tuplesContainUseless(lineToTuples(line))).filter(useless => useless).length
+  console.log(uselessJobsAmount)
+
 });
 
