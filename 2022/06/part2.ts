64fbcaed519d138cc949ba87de0a6a49b79fde57
Sem Pruijs <sem@pruijs.net>
2022-12-12 17:00:51 +0100

    Solve 6.2

---
+++ 2022/06/part2.ts
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+fs.readFile('./input/input.txt', 'utf8', (err: any, data: string) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // cheks if a string has a duplicate character
+  function stringContainsDuplicate(string: string): boolean {
+    const stringWithoutCheckedChar = string.slice(1, Array.from(string).length)
+    return string.length == 2 ? stringWithoutCheckedChar.includes(string[0]) : stringWithoutCheckedChar.includes(string[0]) ? true : stringContainsDuplicate(stringWithoutCheckedChar)
+  }
+
+  function getStartMarker(line: string): number {
+    const markerLength = 14
+    for (let i = 0; i < Array.from(line).length; i++) {
+      if (!stringContainsDuplicate(line.slice(i, i + markerLength))) {
+        return i + markerLength
+      }
+    }
+    return 0
+  }
+
+  const line = data
+  console.log(getStartMarker(line))
+});
+
