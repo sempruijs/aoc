32286f7af3990ac846f3af3f117ed044b683fb14
Sem Pruijs <sem@pruijs.net>
2022-12-12 16:53:53 +0100

    Solve 6.1

--- 2022/06/part1.ts
+++ 2022/06/part1.ts
@@ -6,7 +6,23 @@ fs.readFile('./input/input.txt', 'utf8', (err: any, data: string) => {
     return;
   }
 
+  // cheks if a string has a duplicate character
+  function stringContainsDuplicate(string: string): boolean {
+    const stringWithoutCheckedChar = string.slice(1, Array.from(string).length)
+    return string.length == 2 ? stringWithoutCheckedChar.includes(string[0]) : stringWithoutCheckedChar.includes(string[0]) ? true : stringContainsDuplicate(stringWithoutCheckedChar)
+  }
+
+  function getStartMarker(line: string): number {
+    const markerLength = 4
+    for (let i = 0; i < Array.from(line).length; i++) {
+      if (!stringContainsDuplicate(line.slice(i, i + markerLength))) {
+        return i + markerLength
+      }
+    }
+    return 0
+  }
+
   const line = data
-  console.log(line)
+  console.log(getStartMarker(line))
 });
 
