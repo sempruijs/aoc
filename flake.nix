f6dab757c04be5ea27b4a097dc5ecdd749d7afdc
sempruijs <sem@pruijs.net>
2023-06-29 13:00:24 +0200

    add rust tools to devshell

--- flake.nix
+++ flake.nix
@@ -3,18 +3,34 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
   outputs = { self, nixpkgs, flake-parts, ... }: flake-parts.lib.mkFlake { inherit self; } {
     systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
     perSystem = { config, self', inputs', pkgs, system, ... }: {
+      packages = {
+        rust-stable = inputs'.rust-overlay.packages.rust.override {
+          extensions = [ "rust-src" "rust-analyzer" "clippy" ];
+        };        
+      };
+
       devShells = {
         default =
           pkgs.mkShell {
-            buildInputs = with pkgs; [ nodePackages.typescript nodePackages.typescript-language-server nodePackages.ts-node nodejs bacon rnix-lsp hyperfine cargo-flamegraph cargo];
+            # buildInputs = with pkgs; [ nodePackages.typescript nodePackages.typescript-language-server nodePackages.ts-node nodejs bacon rnix-lsp ];
+              buildInputs = [ self'.packages.rust-stable ]
+                ++ (with pkgs; [ bacon rnix-lsp hyperfine cargo-flamegraph nodePackages.typescript nodePackages.typescript-language-server nodePackages.ts-node nodejs ])
+                ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [ Security ]));
+
           };
       };
     };
