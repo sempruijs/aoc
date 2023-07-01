ac3cd0ae66342f9497cd217d05c25e94cbc0a398
sempruijs <sem@pruijs.net>
2023-07-01 15:54:51 +0200

    finish part-1

--- 2019/01/part-1/src/main.rs
+++ 2019/01/part-1/src/main.rs
@@ -8,15 +8,39 @@ fn read_lines(filename: &str) -> Vec<String> {
         .collect() // gather them together into a vector
 }
 
+fn calc_fual(m: u32) -> u32 {
+    (m as f32 / 3 as f32).floor() as u32 - 2
+}
+
+fn line_to_num(l: String) -> u32 {
+    l.trim().parse().expect("failed to read line")
+}
+
 fn main() {
     let lines = read_lines("input.txt");
-    println!("{}", lines[0]);
+
+    // let nums = map line_to_num lines
+    // let result = sum map calc_fual nums
+    let mut result: u32 = 0;
+
+    for line in lines {
+        let num = line_to_num(line);
+        let fual = calc_fual(num);
+
+        result += fual;
+    }
+
+    println!("{}", result);
 }
 
 #[cfg(test)]
 mod Test {
+    use super::*;
+
     #[test]
-    fn bla() {
-        assert!(true);
+    fn test_fual() {
+        assert_eq!(calc_fual(12), 2);
+        assert_eq!(calc_fual(14), 2);
+        assert_eq!(calc_fual(1969), 654);
     }
 }
