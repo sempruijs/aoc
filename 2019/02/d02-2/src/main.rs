9b743d49feedacf9f30fb11b5bc6083751038ff7
sempruijs <sem@pruijs.net>
2023-07-06 15:08:32 +0200

    finish part 2

--- 2019/02/d02-2/src/main.rs
+++ 2019/02/d02-2/src/main.rs
@@ -1,8 +1,17 @@
 use crate::intcode::*;
+use std::process;
 
 mod intcode;
 
 fn main() {
-    let result = intcode(12, 02);
-    println!("{}", result);
+    for a in 0..99 {
+        for b in 0..99 {
+            if intcode(a, b) == 19690720 {
+                let result = 100 * a + b;
+                println!("--- result found ---\n\n {}", result);
+                process::exit(0);
+            }
+        }
+    }
+    println!("no result found");
 }
