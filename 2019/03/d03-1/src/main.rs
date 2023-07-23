87ef593aa159c50b35546c929286af823dfbdc8d
sempruijs <sem@pruijs.net>
2023-07-23 18:16:44 +0200

    save progress

--- 2019/03/d03-1/src/main.rs
+++ 2019/03/d03-1/src/main.rs
@@ -1,9 +1,12 @@
-enum Steps {
+mod parser;
+
+pub enum Step {
     X(i32),
     Y(i32),
 }
 
-struct Point {
+#[derive(Debug, PartialEq)]
+pub struct Point {
     x: i32,
     y: i32,
 }
@@ -14,7 +17,8 @@ pub enum Dir {
     Vertical,
 }
 
-struct Line {
+#[derive(Debug, PartialEq)]
+pub struct Line {
     p1: Point,
     p2: Point,
 }
@@ -23,11 +27,49 @@ impl Point {
     fn from(x: &i32, y: &i32) -> Point {
         Point { x: *x, y: *y }
     }
+
+    pub fn origin() -> Self {
+        Point::from(&0, &0)
+    }
+}
+
+impl Clone for Point {
+    fn clone(&self) -> Self {
+        Point {
+            x: self.x,
+            y: self.y,
+        }
+    }
+}
+
+impl Step {
+    pub fn to_line(&self, from_point: Point) -> Line {
+        let new_point = match self {
+            Step::X(steps) => Point::from(&(from_point.x + steps), &from_point.y),
+            Step::Y(steps) => Point::from(&from_point.x, &(from_point.y + steps)),
+        };
+
+        Line::from(&from_point, &new_point)
+    }
 }
 
 impl Line {
-    // fn from(p1: &Point, p2: &Point) -> Self {
-    // }
+    fn from(p1: &Point, p2: &Point) -> Self {
+        let dir_line = Line {
+            p1: p1.clone(),
+            p2: p2.clone(),
+        };
+
+        let (small_p, big_p) = match &p1.x > &p2.x || &p1.y > &p2.y {
+            true => (p2, p1),
+            false => (p1, p2),
+        };
+
+        Line {
+            p1: small_p.clone(),
+            p2: big_p.clone(),
+        }
+    }
 
     fn dir(&self) -> Dir {
         if self.p1.x == self.p2.x {
@@ -35,6 +77,19 @@ impl Line {
         }
         Dir::Horizontal
     }
+}
+
+fn steps_to_lines(steps: Vec<Step>) -> Vec<Line> {
+    let mut from_point = Point::origin();
+    let mut lines: Vec<Line> = Vec::new();
+
+    for step in steps {
+        let line = step.to_line(from_point);
+        from_point = line.p2.clone();
+        lines.push(line);
+    }
+
+    lines
 }
 
 fn main() {
@@ -55,5 +110,36 @@ mod tests {
         };
 
         assert_eq!(l1.dir(), Dir::Vertical);
+    }
+
+    #[test]
+    fn test_line_from() {
+        let p1 = Point::from(&0, &0);
+        let p2 = Point::from(&0, &-5);
+        let result = Line::from(&p1.clone(), &p2.clone());
+        assert_eq!(result, Line { p1: p2, p2: p1 });
+    }
+
+    #[test]
+    fn test_step_to_line() {
+        let step = Step::X(5);
+        let from_point = Point::from(&0, &0);
+        let result = step.to_line(from_point.clone());
+        let expected_result = Line::from(&from_point, &Point::from(&5, &0));
+
+        assert_eq!(result, expected_result);
+    }
+
+    #[test]
+    fn test_steps_to_lines() {
+        let steps = vec![Step::X(5), Step::Y(-3)];
+        let result = steps_to_lines(steps);
+        let expected_points = vec![Point::origin(), Point::from(&5, &0), Point::from(&5, &-3)];
+        let expected_result: Vec<Line> = vec![
+            Line::from(&expected_points[0], &expected_points[1]),
+            Line::from(&expected_points[1], &expected_points[2]),
+        ];
+
+        assert_eq!(result, expected_result);
     }
 }
