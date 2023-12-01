e38914fa4c16424901c94338b97ee4975704b7a0
sem pruijs <sem@pruijs.net>
2023-12-01 11:42:29 +0100

    solve part 1

--- 2023/01/d01-1/src/main.rs
+++ 2023/01/d01-1/src/main.rs
@@ -1,3 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("../../input.txt");
+    let answer = input_to_answer(&input);
+    println!("{}", answer);
+}
+
+fn input_to_answer(s: &str) -> u32 {
+    s.lines().map(line_to_calibration_value).sum()
+}
+
+fn line_to_calibration_value(s: &str) -> u32 {
+    let numbers = line_to_numbers(s);
+    numbers_to_calibration_value(numbers)
+}
+
+fn line_to_numbers(s: &str) -> Vec<u32> {
+    let chars: Vec<char> = s.chars().collect();
+    let bla: Vec<char> = chars.into_iter().filter(|&c| c.is_digit(10)).collect();
+    let numbers: Vec<u32> = bla.into_iter().map(|c| c.to_digit(10).unwrap()).collect();
+    numbers
+}
+
+fn numbers_to_calibration_value(xs: Vec<u32>) -> u32 {
+    let first = xs.first().unwrap();
+    let last = xs.last().unwrap();
+    (first * 10) + last
 }
