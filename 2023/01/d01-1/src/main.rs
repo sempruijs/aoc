da553f433da3e8b63aa43d3ee4bd206ba24c3a64
sem pruijs <sem@pruijs.net>
2023-12-01 11:45:58 +0100

    refactor line_to_numbers

--- 2023/01/d01-1/src/main.rs
+++ 2023/01/d01-1/src/main.rs
@@ -14,10 +14,11 @@ fn line_to_calibration_value(s: &str) -> u32 {
 }
 
 fn line_to_numbers(s: &str) -> Vec<u32> {
-    let chars: Vec<char> = s.chars().collect();
-    let bla: Vec<char> = chars.into_iter().filter(|&c| c.is_digit(10)).collect();
-    let numbers: Vec<u32> = bla.into_iter().map(|c| c.to_digit(10).unwrap()).collect();
-    numbers
+    s.chars()
+        .into_iter()
+        .filter(|&c| c.is_digit(10))
+        .map(|c| c.to_digit(10).unwrap())
+        .collect()
 }
 
 fn numbers_to_calibration_value(xs: Vec<u32>) -> u32 {
