ea82c5184abb1dd98cfab4056c2db04fbca2b6ce
sem pruijs <sem@pruijs.net>
2023-12-03 21:10:52 +0100

    save progress

--- 2023/03/d03-1/src/main.rs
+++ 2023/03/d03-1/src/main.rs
@@ -1,4 +1,29 @@
+struct Matrix(Vec<Vec<char>>);
+struct Point {
+    x: u32,
+    y: u32,
+}
+
 fn main() {
     let input = include_str!("../../input.txt");
     println!("{}", input);
+}
+
+impl Matrix {
+    pub fn from(s: &str) -> Self {
+        Matrix(
+            s.lines()
+                .map(|line| line.chars().collect::<Vec<char>>())
+                .collect::<Vec<Vec<char>>>(),
+        )
+    }
+
+fn bind_points(&self) -> Vec<Point> {
+    //     for y in self.0 {
+    //     }        
+    // }
+}
+
+fn char_can_bind(c: &char) -> bool {
+    (!c.is_digit(10)) && c != &'.'
 }
