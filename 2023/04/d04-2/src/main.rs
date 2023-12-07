069d1ad950c6bdefd13b9f4bf108d6e5b6e21d56
sem pruijs <sem@pruijs.net>
2023-12-07 20:14:47 +0100

    save progress

--- 2023/04/d04-2/src/main.rs
+++ 2023/04/d04-2/src/main.rs
@@ -7,7 +7,9 @@ fn main() {
 }
 
 fn puzzle_input_to_result(s: &str) -> u32 {
-    s.lines().map(|s| Card::from_line(s).points()).sum()
+    // s.lines().map(|s| Card::from_line(s).points()).sum()
+    let cards: Vec<Card> = s.lines().map(|s| Card::from_line(s)).collect();
+    cards_to_copy_amount(cards.as_slice(), Vec::new(), 1)
 }
 
 struct Card {
@@ -43,9 +45,39 @@ impl Card {
             .collect::<Vec<&u32>>()
             .len() as u32;
 
-        return match amount_of_winning_numbers == 0 {
-            true => 0,
-            false => (2 as u32).pow(amount_of_winning_numbers - 1),
-        };
+        amount_of_winning_numbers
+    }
+}
+
+fn distribute_over(x: u32, copies: Vec<u32>) -> Vec<u32> {
+    let mut result = copies;
+
+    for i in 0..x {
+        let i = i as usize;
+        if let _ = result[i] {
+            result[i] += 1;
+        } else {
+            result.push(2);
+        }
+    }
+    result
+}
+
+// [Cards] -> [u32] -> u32 -> u32
+fn cards_to_copy_amount(cards: &[Card], copies: Vec<u32>, n: u32) -> u32 {
+    // are cards
+    let (c, cs) = (&cards[0], &cards[1..]);
+
+    // are copies
+    let (x, xs) = (&copies[0], &copies[1..]);
+
+    let points = c.points();
+    let copies = distribute_over(points, xs.to_vec());
+
+    if copies.iter().sum::<u32>() == 0 {
+        // base case
+        return n;
+    } else {
+        cards_to_copy_amount(cs, copies, n + x)
     }
 }
