d7ae571fd0ca97174eb2cc22f037bb18dc012b51
sem pruijs <sem@pruijs.net>
2023-12-25 09:50:33 +0100

    init day 25

---
+++ 2023/25/d25-1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = include_str!("../../example.txt");
+    let answer = puzzle_input_to_answer(input);
+    println!("{}", answer);
+}
+
+fn puzzle_input_to_answer(s: &str) -> u32 {
+    todo!()
+}
