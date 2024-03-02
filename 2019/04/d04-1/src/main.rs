15b0042e2c84b24829a9f70dcf0214f127b2e14d
sem pruijs <sem@pruijs.net>
2024-03-02 12:44:33 +0100

    solve part 1

--- 2019/04/d04-1/src/main.rs
+++ 2019/04/d04-1/src/main.rs
@@ -4,6 +4,20 @@ fn main() {
     println!("{}", answer);
 }
 
+#[derive(Clone, PartialEq, Eq, Debug)]
+struct Digits(Vec<u32>);
+
+impl From<u32> for Digits {
+    fn from(n: u32) -> Self {
+        let digits: Vec<u32> = n
+            .to_string()
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .collect();
+        Self(digits)
+    }
+}
+
 fn input_to_range(s: &str) -> Vec<u32> {
     let (start, end) = s.split_once("-").unwrap();
     let start: u32 = start.parse().unwrap();
@@ -11,12 +25,37 @@ fn input_to_range(s: &str) -> Vec<u32> {
     (start..end).collect()
 }
 
-fn puzzle_input_to_result(s: &str) -> u32 {
-    input_to_range(s).into_iter().filter(valid_number).sum()
+fn puzzle_input_to_result(s: &str) -> usize {
+    input_to_range(s).into_iter().filter(valid_number).count()
 }
 
 fn valid_number(n: &u32) -> bool {
-    true
+    let digits = Digits::from(*n);
+    digits.has_nabour() && digits.does_increase()
+}
+
+impl Digits {
+    fn does_increase(&self) -> bool {
+        let mut acc = 0;
+        for n in &self.0 {
+            if &acc > n {
+                return false;
+            }
+            acc = *n;
+        }
+        true
+    }
+
+    fn has_nabour(&self) -> bool {
+        let mut acc = 42;
+        for n in &self.0 {
+            if &acc == n {
+                return true;
+            }
+            acc = *n;
+        }
+        false
+    }
 }
 
 #[cfg(test)]
@@ -28,6 +67,14 @@ mod tests {
         let input_1 = "1-3";
         let expected_1 = vec![1, 2];
         let result_1 = input_to_range(input_1);
+        assert_eq!(result_1, expected_1);
+    }
+
+    #[test]
+    fn test_to_digits() {
+        let input_1 = 123;
+        let expected_1 = Digits(vec![1, 2, 3]);
+        let result_1 = Digits::from(input_1);
         assert_eq!(result_1, expected_1);
     }
 }
