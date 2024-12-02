911536bd5cf04df4f3072bd5c714620c688b6654
sem pruijs <sem@pruijs.net>
2024-12-02 08:55:41 +0100

    init day 2 part 1

---
+++ 2024/02/d02-1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = include_str!("../../example.txt");
+    let answer = input_to_answer(&input);
+    println!("{}", answer);
+}
+
+fn input_to_answer(s: &str) -> u32 {
+    0
+}
