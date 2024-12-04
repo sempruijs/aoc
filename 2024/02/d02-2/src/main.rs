67bbb8d1dc95223403726b41552fff8ac659bbde
sem pruijs <sem@pruijs.net>
2024-12-04 23:42:55 +0100

    improve day 2 part 2

--- 2024/02/d02-2/src/main.rs
+++ 2024/02/d02-2/src/main.rs
@@ -29,54 +29,21 @@ fn input_to_reports(s: &str) -> Vec<Report> {
     s.lines().map(line_to_report).collect()
 }
 
-#[derive(PartialEq, Eq, Debug)]
-enum Direction {
-    Decreasing,
-    Increasing,
-}
-
-struct Pair(i32, i32);
-
-impl Pair {
-    fn dir(&self) -> Direction {
-        match self.0 > self.1 {
-            true => Direction::Decreasing,
-            false => Direction::Increasing,
-        }
-    }
-
-    fn valid_difference(&self) -> bool {
-        let difference = match self.0 > self.1 {
-            true => self.0 - self.1,
-            false => self.1 - self.0,
-        };
-        difference > 0 && difference < 4
-    }
-}
-
 impl Report {
-    fn to_pairs(&self) -> Vec<Pair> {
-        self.0
-            .clone()
-            .into_iter()
-            .tuple_windows()
-            .map(|(p, c)| Pair(p, c))
-            .collect()
-    }
-
     fn valid(&self) -> bool {
-        let pairs = &self.to_pairs();
-        let correct_difference = pairs.iter().fold(true, |result, p| match result {
-            true => p.valid_difference(),
-            false => false,
+        let decrease = self.0.iter().is_sorted_by(|x, y| {
+            let diff = **x - **y;
+            diff < 4 && diff > 0
         });
-        let correct_direction = pairs.iter().all(|p| p.dir() == Direction::Decreasing)
-            || pairs.iter().all(|p| p.dir() == Direction::Increasing);
-        correct_difference && correct_direction
+        let increase = self.0.iter().is_sorted_by(|x, y| {
+            let diff = **y - **x;
+            diff < 4 && diff > 0
+        });
+        increase || decrease
     }
 
     fn valid_with_one_mistake(&self) -> bool {
-        let r = &self.clone();
+        let r = self;
         self.0
             .clone()
             .iter()
