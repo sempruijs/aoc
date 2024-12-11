df1ca2cd4d2eeea03adb517c6bb097f0305aa1ee
sem pruijs <sem@pruijs.net>
2024-12-11 09:37:18 +0100

    Init day 11 part 1

---
+++ 2024/11/d11-1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = include_str!("../../example.txt");
+    let answer = input_to_answer(input);
+    println!("")
+}
+
+fn input_to_answer(s: &str) -> u128 {
+    todo!()
+}
