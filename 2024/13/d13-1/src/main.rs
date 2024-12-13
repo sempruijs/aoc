667c9928c40996a285b16669f51ea5fa489727f5
sem pruijs <sem@pruijs.net>
2024-12-13 19:25:17 +0100

    init day 13 part 1

---
+++ 2024/13/d13-1/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let input = include_str!("../../example.txt");
+    let answer = input_to_answer(input);
+    println!("answer is: {}", answer);
+}
+
+fn input_to_answer(s: &str) -> usize {
+    todo!()
+}
