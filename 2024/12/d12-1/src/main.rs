3292693564effa605e14a92b86884aaf0b3ef935
sem pruijs <sem@pruijs.net>
2024-12-13 10:55:40 +0100

    solve day 12 part 1

--- 2024/12/d12-1/src/main.rs
+++ 2024/12/d12-1/src/main.rs
@@ -1,6 +1,8 @@
 use rayon::prelude::*;
+use std::collections::HashMap;
 use std::collections::HashSet;
 
+// this is all horible code. Please forgive me.
 fn main() {
     let input = include_str!("../../input.txt");
     let answer = input_to_answer(input);
@@ -8,7 +10,8 @@ fn main() {
 }
 
 fn input_to_answer(s: &str) -> usize {
-    let shapes = Shapes::try_from(s).unwrap().merge();
+    let world = World::try_from(s).unwrap();
+    let shapes = Shapes::from(world);
     shapes.fence_prices()
 }
 
@@ -52,35 +55,79 @@ impl TryFrom<&str> for Shapes {
     }
 }
 
-impl Shapes {
-    fn merge(mut self) -> Self {
-        for (j, shape1) in self.0.iter().enumerate() {
-            for (i, shape2) in self.0.iter().enumerate() {
-                if shape1.c == shape2.c && shape1 != shape2 {
-                    for point in shape1.points.clone() {
-                        if shape2.neighbor_count(&point) > 0 {
-                            let points: HashSet<Point> = shape1
-                                .clone()
-                                .points
-                                .union(&shape2.points.clone())
-                                .cloned()
-                                .collect();
-                            let shape = Shape {
-                                points: points,
-                                c: shape1.c,
-                            };
-                            self.0.remove(i);
-                            self.0.remove(j);
-                            self.0.push(shape);
-                            return self.merge();
-                        }
-                    }
-                }
+#[derive(Debug, Clone)]
+struct World(HashMap<Point, char>);
+
+impl TryFrom<&str> for World {
+    type Error = ();
+
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        let mut hm = HashMap::new();
+        s.lines().enumerate().for_each(|(y, l)| {
+            l.chars().enumerate().for_each(|(x, c)| {
+                let point = Point {
+                    x: x as i32,
+                    y: y as i32,
+                };
+                hm.insert(point, c);
+            })
+        });
+        Ok(Self(hm))
+    }
+}
+
+impl From<World> for Shapes {
+    fn from(w: World) -> Self {
+        let mut w = w.clone();
+        let mut shapes = Vec::new();
+        while !w.0.is_empty() {
+            if let Some(shape) = w.get_first_shape() {
+                w = w.filter(&shape);
+                shapes.push(shape);
             }
         }
-        self
+        Self(shapes)
+    }
+}
+
+impl World {
+    fn filter(&self, shape: &Shape) -> Self {
+        let mut hm = self.0.clone();
+        shape.points.iter().for_each(|p| {
+            hm.remove(p);
+        });
+        Self(hm)
     }
 
+    fn get_first_shape(&self) -> Option<Shape> {
+        let (p, c) = self.0.iter().next()?;
+        let mut visited = HashSet::from([p.clone()]);
+        self.make_shape(p, &mut visited, *c);
+        Some(Shape {
+            points: visited,
+            c: *c,
+        })
+    }
+}
+
+impl World {
+    fn make_shape(&self, p: &Point, mut visited: &mut HashSet<Point>, c: char) {
+        let top = Point { x: p.x, y: p.y - 1 };
+        let bottom = Point { x: p.x, y: p.y + 1 };
+        let right = Point { x: p.x + 1, y: p.y };
+        let left = Point { x: p.x - 1, y: p.y };
+        vec![top, right, bottom, left].iter().for_each(|p| {
+            if let Some(d) = self.0.get(&p) {
+                if *d == c && !visited.contains(&p) {
+                    visited.insert(p.clone());
+                    self.make_shape(p, &mut visited, c);
+                }
+            }
+        });
+    }
+}
+
+impl Shapes {
     fn fence_prices(&self) -> usize {
         let ns: Vec<usize> = self.0.par_iter().map(Shape::fence_price).collect();
         ns.iter().sum()
