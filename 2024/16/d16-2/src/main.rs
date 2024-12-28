6a5e4db6cd5bd43a2525841a12f78d1f52ab55f4
sem pruijs <sem@pruijs.net>
2024-12-28 19:27:09 +0100

    solve day 16 part 2

--- 2024/16/d16-2/src/main.rs
+++ 2024/16/d16-2/src/main.rs
@@ -1,20 +1,35 @@
-use petgraph::algo::{all_simple_paths, dijkstra, min_spanning_tree};
-use petgraph::csr::IndexType;
-use petgraph::data::FromElements;
-use petgraph::dot::{Config, Dot};
+// #![allow(warnings)]
+
+use petgraph::algo::dijkstra;
 use petgraph::graph::{NodeIndex, UnGraph};
+use std::collections::HashMap;
 use std::collections::HashSet;
 use std::fmt::Display;
 
 fn main() {
-    let input = include_str!("../../example.txt");
+    let input = include_str!("../../input.txt");
     let answer = input_to_answer(input);
-    println!("The answer is: {answer}");
+    println!("The answer is: {}", answer);
 }
 
 impl Display for Line {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{} <-> {}", self.p1, self.p2)
+    }
+}
+
+impl Display for Path {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let result =
+            self.0
+                .clone()
+                .into_iter()
+                .map(Point::from)
+                .fold(String::new(), |mut result, p| {
+                    result.push_str(&format!(" {p}"));
+                    result
+                });
+        write!(f, "{}", result)
     }
 }
 
@@ -52,15 +67,21 @@ impl Display for Dir {
 }
 
 struct World {
-    g: UnGraph<u32, u32>,
+    g: UnGraph<Point, u32>,
     start: Point,
     end: Point,
 }
 
-#[derive(Default, Debug, Clone, PartialEq, Eq, Hash)]
+#[derive(Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
-    x: u32,
-    y: u32,
+    x: u16,
+    y: u16,
+}
+
+impl From<Point> for NodeIndex {
+    fn from(p: Point) -> Self {
+        (((p.x as u32) << 16) | (p.y as u32)).into()
+    }
 }
 
 impl Point {
@@ -84,10 +105,6 @@ impl Point {
             },
         }
     }
-
-    fn index(&self) -> u32 {
-        self.x * 100 + self.y
-    }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -96,13 +113,48 @@ struct Line {
     p2: Point,
 }
 
+impl From<NodeIndex> for Point {
+    fn from(value: NodeIndex) -> Self {
+        let value: u32 = value.index().try_into().unwrap();
+        let x = (value >> 16) as u16;
+        let y = (value & 0xFFFF_FFFF) as u16;
+        Self { x, y }
+    }
+}
+
 impl Line {
     fn cost(&self) -> u32 {
-        self.p1.x.abs_diff(self.p2.x) + self.p1.y.abs_diff(self.p2.y) + 1000
+        (self.p1.x.abs_diff(self.p2.x) + self.p1.y.abs_diff(self.p2.y) + 1000) as u32
     }
 
-    fn edge(&self) -> (u32, u32, u32) {
-        (self.p1.index(), self.p2.index(), self.cost())
+    fn edge(&self) -> (Point, Point, u32) {
+        (self.p1.clone(), self.p2.clone(), self.cost())
+    }
+
+    fn points(&self) -> Vec<Point> {
+        // determen direction
+        match self.p1.x == self.p2.x {
+            true => {
+                // vertical
+                let (small, big) = match self.p1.y < self.p2.y {
+                    true => (self.p1, self.p2),
+                    false => (self.p2, self.p1),
+                };
+                return (small.y..=big.y)
+                    .map(|y| Point { x: small.x, y: y })
+                    .collect();
+            }
+            false => {
+                // horizontal
+                let (small, big) = match self.p1.x < self.p2.x {
+                    true => (self.p1, self.p2),
+                    false => (self.p2, self.p1),
+                };
+                return (small.x..=big.x)
+                    .map(|x| Point { x: x, y: small.y })
+                    .collect();
+            }
+        }
     }
 }
 
@@ -224,7 +276,7 @@ impl From<Corners> for Lines {
 }
 
 impl Corners {
-    fn filter_on_x(&self, x: u32) -> Self {
+    fn filter_on_x(&self, x: u16) -> Self {
         Self(
             self.0
                 .clone()
@@ -234,7 +286,7 @@ impl Corners {
         )
     }
 
-    fn filter_on_y(&self, y: u32) -> Self {
+    fn filter_on_y(&self, y: u16) -> Self {
         Self(
             self.0
                 .clone()
@@ -250,18 +302,18 @@ impl TryFrom<&str> for World {
 
     fn try_from(s: &str) -> Result<Self, Self::Error> {
         let corners = Corners::try_from(s).unwrap();
-        let edges: &[(u32, u32, u32)] = &Lines::from(corners)
+        let edges: &[(Point, Point, u32)] = &Lines::from(corners)
             .0
             .into_iter()
             .map(|l| l.edge())
-            .collect::<Vec<(u32, u32, u32)>>();
-        let g = UnGraph::<u32, u32>::from_edges(edges);
+            .collect::<Vec<(Point, Point, u32)>>();
+        let g = UnGraph::<Point, u32>::from_edges(edges);
         let start = Point {
             x: 1,
-            y: s.lines().count() as u32 - 2,
+            y: s.lines().count() as u16 - 2,
         };
         let end = Point {
-            x: s.lines().next().unwrap().chars().count() as u32 - 2,
+            x: s.lines().next().unwrap().chars().count() as u16 - 2,
             y: 1,
         };
         Ok(Self {
@@ -286,12 +338,30 @@ impl Path {
         self.0.contains(i)
     }
 
-    fn len(&self, w: &World) -> u32 {
-        todo!()
+    fn cost(&self, w: &World) -> u32 {
+        let slice = &self.0[..];
+        slice
+            .windows(2)
+            .into_iter()
+            .map(|window| {
+                w.g.edge_weight(w.g.find_edge(window[0], window[1]).unwrap())
+                    .unwrap()
+            })
+            .sum()
     }
 
-    fn points(&self, w: &World) -> Vec<Point> {
-        todo!()
+    fn points(&self) -> Vec<Point> {
+        let slice = &self.0[..];
+        slice
+            .windows(2)
+            .flat_map(|window| {
+                Line {
+                    p1: window[0].into(),
+                    p2: window[1].into(),
+                }
+                .points()
+            })
+            .collect()
     }
 
     fn last(&self) -> NodeIndex {
@@ -300,9 +370,18 @@ impl Path {
 }
 
 impl World {
-    fn shortest_paths(&self, path: Path, max_len: u32) -> Vec<Path> {
-        if path.len(self) > max_len {
+    fn shortest_paths(
+        &self,
+        path: Path,
+        max_len: u32,
+        dijkstra: &HashMap<NodeIndex, u32>,
+    ) -> Vec<Path> {
+        let cost = path.cost(self);
+        if cost > max_len || dijkstra.get(&path.last()).unwrap() < &cost {
             return Vec::new();
+        }
+        if cost == max_len && path.last() == NodeIndex::from(self.end.clone()) {
+            return vec![path];
         }
         self.g
             .neighbors(path.last())
@@ -310,30 +389,85 @@ impl World {
                 true => vec![],
                 false => {
                     let new_path = path.clone().push(neighbor);
-                    self.shortest_paths(new_path, max_len)
+                    self.shortest_paths(new_path, max_len, dijkstra)
                 }
             })
             .collect()
     }
 
     fn answer(&self) -> u32 {
-        let len = self.shortest_path_len();
-        let start_path = Path(vec![self.start.index().into()]);
-        self.shortest_paths(start_path, len)
+        let dijkstra = dijkstra(&self.g, self.start.into(), None, |e| *e.weight());
+        let len = dijkstra.get(&self.end.into()).unwrap();
+        println!("{len}");
+        let start_path = Path(vec![self.start.into()]);
+        self.shortest_paths(start_path, *len, &dijkstra)
             .into_iter()
-            .flat_map(|p| p.points(&self))
+            .flat_map(|p| p.points())
             .collect::<HashSet<Point>>()
             .len() as u32
-    }
-
-    fn shortest_path_len(&self) -> u32 {
-        let node_map = dijkstra(&self.g, self.start.index().into(), None, |e| *e.weight());
-        *node_map
-            .get(&NodeIndex::new(self.end.index().try_into().unwrap()))
-            .unwrap()
     }
 }
 
 fn input_to_answer(s: &str) -> u32 {
     World::try_from(s).unwrap().answer()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_line_to_points() {
+        let input = Line {
+            p1: Point { x: 0, y: 0 },
+            p2: Point { x: 0, y: 3 },
+        };
+        let expected = vec![
+            Point { x: 0, y: 0 },
+            Point { x: 0, y: 1 },
+            Point { x: 0, y: 2 },
+            Point { x: 0, y: 3 },
+        ];
+        let result = input.points();
+        assert_eq!(result, expected);
+
+        let input = Line {
+            p1: Point { x: 0, y: 3 },
+            p2: Point { x: 0, y: 0 },
+        };
+        let expected = vec![
+            Point { x: 0, y: 0 },
+            Point { x: 0, y: 1 },
+            Point { x: 0, y: 2 },
+            Point { x: 0, y: 3 },
+        ];
+        let result = input.points();
+        assert_eq!(result, expected);
+
+        let input = Line {
+            p1: Point { x: 0, y: 0 },
+            p2: Point { x: 3, y: 0 },
+        };
+        let expected = vec![
+            Point { x: 0, y: 0 },
+            Point { x: 1, y: 0 },
+            Point { x: 2, y: 0 },
+            Point { x: 3, y: 0 },
+        ];
+        let result = input.points();
+        assert_eq!(result, expected);
+
+        let input = Line {
+            p1: Point { x: 3, y: 0 },
+            p2: Point { x: 0, y: 0 },
+        };
+        let expected = vec![
+            Point { x: 0, y: 0 },
+            Point { x: 1, y: 0 },
+            Point { x: 2, y: 0 },
+            Point { x: 3, y: 0 },
+        ];
+        let result = input.points();
+        assert_eq!(result, expected);
+    }
 }
