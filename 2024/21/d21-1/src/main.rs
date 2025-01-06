372bb1b980e1ec5e5e0d2e7f22d7af744cc8f499
sem pruijs <sem@pruijs.net>
2025-01-06 19:50:56 +0100

    save progress

--- 2024/21/d21-1/src/main.rs
+++ 2024/21/d21-1/src/main.rs
@@ -15,7 +15,62 @@ fn input_to_answer(s: &str) -> usize {
 
 impl Default for World {
     fn default() -> Self {
-        todo!()
+        Self {
+            base: NumPadRoom::default(),
+            layers: vec![
+                DPadRoom::default(),
+                DPadRoom::default(),
+                DPadRoom::default(),
+            ],
+        }
+    }
+}
+
+impl Default for NumPadRoom {
+    fn default() -> Self {
+        let mut hm = HashMap::new();
+
+        // 7 8 9
+        hm.insert(NumPadKey::Seven, Point { x: 0, y: 0 });
+        hm.insert(NumPadKey::Eight, Point { x: 1, y: 0 });
+        hm.insert(NumPadKey::Nine, Point { x: 2, y: 0 });
+
+        // 4 5 6
+        hm.insert(NumPadKey::Four, Point { x: 0, y: 1 });
+        hm.insert(NumPadKey::Five, Point { x: 1, y: 1 });
+        hm.insert(NumPadKey::Six, Point { x: 2, y: 1 });
+
+        // 1 2 3
+        hm.insert(NumPadKey::One, Point { x: 0, y: 2 });
+        hm.insert(NumPadKey::Two, Point { x: 1, y: 2 });
+        hm.insert(NumPadKey::Three, Point { x: 2, y: 2 });
+
+        // - 0 A
+        hm.insert(NumPadKey::Zero, Point { x: 1, y: 3 });
+        hm.insert(NumPadKey::Press, Point { x: 2, y: 3 });
+
+        let arm = Arm(hm.get(&NumPadKey::Press).unwrap().clone());
+
+        Self { key_pad: hm, arm }
+    }
+}
+
+impl Default for DPadRoom {
+    fn default() -> Self {
+        let mut hm = HashMap::new();
+
+        // - ^ A
+        hm.insert(Instruction::North, Point { x: 1, y: 0 });
+        hm.insert(Instruction::Press, Point { x: 2, y: 0 });
+
+        // < v >
+        hm.insert(Instruction::West, Point { x: 0, y: 1 });
+        hm.insert(Instruction::South, Point { x: 1, y: 1 });
+        hm.insert(Instruction::East, Point { x: 2, y: 1 });
+
+        let arm = Arm(hm.get(&Instruction::Press).unwrap().clone());
+
+        Self { key_pad: hm, arm }
     }
 }
 
@@ -23,7 +78,34 @@ impl TryFrom<&str> for Codes {
     type Error = ();
 
     fn try_from(s: &str) -> Result<Self, Self::Error> {
-        todo!()
+        Ok(Self(
+            s.lines().map(|l| Code::try_from(l).unwrap()).collect(),
+        ))
+    }
+}
+
+impl TryFrom<&str> for Code {
+    type Error = ();
+
+    fn try_from(s: &str) -> Result<Self, Self::Error> {
+        Ok(Self(
+            s.chars()
+                .map(|c| match c {
+                    '0' => NumPadKey::Zero,
+                    '1' => NumPadKey::One,
+                    '2' => NumPadKey::Two,
+                    '3' => NumPadKey::Three,
+                    '4' => NumPadKey::Four,
+                    '5' => NumPadKey::Five,
+                    '6' => NumPadKey::Six,
+                    '7' => NumPadKey::Seven,
+                    '8' => NumPadKey::Eight,
+                    '9' => NumPadKey::Nine,
+                    'A' => NumPadKey::Press,
+                    c => panic!("Enknown character {c}"),
+                })
+                .collect(),
+        ))
     }
 }
 
